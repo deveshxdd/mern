f3c4757a1452293f3fc7326deff30193d24774af
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2024-05-24 22:38:36 +0530

    juer

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -12,12 +12,11 @@ declare global {
   }
 }
 
-const jwtCheck = auth({
-  audience: 'mern-food-ordering-app-api',
-  issuerBaseURL: 'https://dev-cmcabayy62227581.us.auth0.com/',
-  tokenSigningAlg: 'RS256'
+export const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  tokenSigningAlg: "RS256",
 });
-
 export const jwtParse = async (
   req: Request,
   res: Response,
